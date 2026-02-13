35410262967de41c6ee4d53ff99c3924a02e4387
DANIELSABRI <147437278+DANIELSABRI@users.noreply.github.com>
2026-02-13 08:52:50 +0530

    Add files via upload
    Configuration file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// ======================================
+// GLOBAL CONFIGURATION
+// ======================================
+
+// CHANGE ONLY IF FOLDER NAME CHANGES
+define('BASE_URL', 'http://localhost/lost-found');
